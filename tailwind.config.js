013847ac8c64f731d15672864a127c51eb781a0f
edusal100 <edusal100@hotmail.com>
2023-08-27 09:49:11 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'red-hat': ['Red Hat Display', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
